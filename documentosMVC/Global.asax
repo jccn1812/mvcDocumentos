2035dfc4d46b8b9c7ddafbb0622b3e6b2ac21e1d
Jc <Jc@JUANCARLOSCB5C8>
2017-09-26 09:14:00 -0300

    Add project files.

---
+++ documentosMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="documentosMVC.MvcApplication" Language="C#" %>
---
+++ documentosMVC/Scripts/includes/Layout.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+
+    $("#buscador").hide();
+
+    $("#btnBuscar").click(function(){
+
+        if ($("#buscador").is(":visible")) {
+            $("#buscador").fadeOut();
+           
+        } else {
+            $("#txtSearch").val("");
+            $("#buscador").fadeIn();       
+        }
+
+
+    })
+
+
+
+
+})
